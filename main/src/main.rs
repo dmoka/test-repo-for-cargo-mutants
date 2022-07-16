25125d9b9f011dd937a401220c103d916c214f14
dmoka <mokabme@gmail.com>
2022-07-16 18:55:19 +0200

    Init commit for the codebase having the issue with mutants crate

---
+++ main/src/main.rs
@@ -0,0 +1,7 @@
+use utils::a_public_module::*;
+
+fn main() {
+    println!("Print works from main binary");
+
+    print_from_utils();
+}
---
+++ utils/src/lib.rs
@@ -0,0 +1,6 @@
+
+pub mod a_public_module {
+    pub fn print_from_utils() {
+        println!("Print works from utils library");
+    }
+}
