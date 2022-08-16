a15d19c8cda11aa234c9e96ac06cfc60cdcb9da3
dmoka <mokabme@gmail.com>
2022-08-16 09:15:41 +0200

    add delete statement binary

---
+++ delete-statement/src/main.rs
@@ -0,0 +1,29 @@
+use std::fmt;
+
+#[cfg(test)]
+use mutagen::mutate;
+
+pub struct EventBus {
+
+}
+
+impl EventBus {
+
+    pub fn deposit_event(event_id: u32) {
+        print!("event with id {} is emitted", event_id);
+    }
+
+    pub fn do_logic(event_id: u32) {
+        //some logic
+        Self::deposit_event(3);
+    }
+}
+
+fn main() {
+}
+
+
+#[test]
+fn test1() {
+    assert!(true);
+}
