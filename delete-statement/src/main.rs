23b55f6e0d4da019b7322c141b7c56658d34109f
dmoka <mokabme@gmail.com>
2022-08-18 08:56:45 +0200

    adjust delete-statement logic to make it more meaningfull

--- delete-statement/src/main.rs
+++ delete-statement/src/main.rs
@@ -1,18 +1,51 @@
 use std::fmt;
 
-pub struct EventBus {
+#[cfg(test)]
+use mutagen::mutate;
+
+
+pub struct Repository {
+    pub data: Vec<String>
+}
+
+impl Repository {
+
+    pub fn new() -> Self {
+        Repository{
+            data: Vec::<String>::new()
+        }
+    }
+
+    pub fn store(&mut self, data_entry: &str) {
+        self.data.push(data_entry.to_string());
+    }
 
 }
 
-impl EventBus {
+pub struct Main {}
 
-    pub fn deposit_event(event_id: u32) {
-        print!("event with id {} is emitted", event_id);
+impl Main {
+
+    #[cfg_attr(test, mutate)]
+    pub fn do_logic(repo: &mut Repository, mut number: u32) -> u32 {
+        number = number + 5;
+
+        repo.store("data from do_logic");
+
+        number = number + 10;
+
+        number
     }
 
-    pub fn do_logic(event_id: u32) {
-        //some logic
-        Self::deposit_event(3);
+    #[cfg_attr(test, mutate)]
+    pub fn do_logic2(repo: &mut Repository, mut number: u32) -> u32 {
+        number = number + 5;
+
+        repo.store("data from do_logic");
+
+        number = number + 20;
+
+        number
     }
 }
 
@@ -21,6 +54,51 @@ fn main() {
 
 
 #[test]
-fn test1() {
-    assert!(true);
+fn do_logic_should_increment_number_with_15() {
+    //Arrange
+    let mut repo = Repository{
+        data: Vec::<String>::new()
+    };
+
+    let mut number: u32 = 5;
+
+    //Act
+    let modified_number = Main::do_logic(&mut repo, number);
+
+    //Assert
+    assert_eq!(modified_number, number + 15);
+}
+
+
+#[test]
+fn do_logic_should_store_data() {
+    //Arrange
+    let mut repo = Repository{
+        data: Vec::<String>::new()
+    };
+
+    let mut number: u32 = 5;
+
+    //Act
+    let modified_number = Main::do_logic(&mut repo, number);
+
+    //Assert
+    assert_eq!(&repo.data[0], "data from do_logic");
+}
+
+
+#[test]
+fn do_logic2_should_increment_number_with_25() {
+    //Arrange
+    let mut repo = Repository{
+        data: Vec::<String>::new()
+    };
+
+    let mut number: u32 = 5;
+
+    //Act
+    let modified_number = Main::do_logic2(&mut repo, number);
+
+    //Assert
+    assert_eq!(modified_number, number + 25);
 }
